f5cad20e6a8a8571c4a601d555fdfee7629b6389
Olldar <122790866+Olldar@users.noreply.github.com>
2023-11-26 19:29:59 +0300

    Update ParseCitySubjson.cpp
    Сделал все как было изначально, с минимальными комментами

--- ParseCitySubjson.cpp
+++ ParseCitySubjson.cpp
@@ -1,39 +1,30 @@
-City ParseCity(const Json& city_json, const string& country_phone_code, const vector<Language>& languages) {
-    return {
-            city_json["name"s].AsString(),
-            city_json["iso_code"s].AsString(),
-            country_phone_code + city_json["phone_code"s].AsString(),
-            languages
-    };
-}
-
-Country ParseCountry(const Json& country_json) {
-    Country country{
-            country_json["name"s].AsString(),
-            country_json["iso_code"s].AsString(),
-            country_json["phone_code"s].AsString(),
-            country_json["time_zone"s].AsString()
-    };
-
-    for (const auto& langJson : country_json["languages"s].AsList()) {
-        country.languages.push_back(FromString<Language>(langJson.AsString()));
-    }
-
-    return country;
-}
-
-void ParseCitySubjson(vector<City>& cities, const Json& json, const string& countryPhoneCode, const vector<Language>& languages) {
-    for (const auto& cityJson : json.AsList()) {
-        cities.push_back(ParseCity(cityJson.AsObject(), countryPhoneCode, languages));
+// Дана функция ParseCitySubjson, обрабатывающая JSON-объект со списком городов конкретной страны:
+void ParseCitySubjson(vector<City>& cities, const Json& json, const string& country_name,
+                      const string& country_iso_code, const string& country_phone_code, const string& country_time_zone,
+                      const vector<Language>& languages) {
+    for (const auto& city_json : json.AsList()) {
+        const auto& city_obj = city_json.AsObject();
+        cities.push_back({city_obj["name"s].AsString(), city_obj["iso_code"s].AsString(),
+                          country_phone_code + city_obj["phone_code"s].AsString(), country_name, country_iso_code,
+                          country_time_zone, languages});
     }
 }
 
+// ParseCitySubjson вызывается только из функции ParseCountryJson следующим образом:
 void ParseCountryJson(vector<Country>& countries, vector<City>& cities, const Json& json) {
-    for (const auto& countryJson : json.AsList()) {
-        Country country = ParseCountry(countryJson.AsObject());
-        countries.push_back(country);
-        ParseCitySubjson(cities, countryJson["cities"s], country.phone_code, country.languages);
+    for (const auto& country_json : json.AsList()) {
+        const auto& country_obj = country_json.AsObject();
+        countries.push_back({
+            country_obj["name"s].AsString(),
+            country_obj["iso_code"s].AsString(),
+            country_obj["phone_code"s].AsString(),
+            country_obj["time_zone"s].AsString(),
+        });
+        Country& country = countries.back();
+        for (const auto& lang_obj : country_obj["languages"s].AsList()) {
+            country.languages.push_back(FromString<Language>(lang_obj.AsString()));
+        }
+        ParseCitySubjson(cities, country_obj["cities"s], country.name, country.iso_code, country.phone_code,
+                         country.time_zone, country.languages);
     }
 }
-//Я выделил логику создания объектов City и Country в отдельные функции ParseCity и ParseCountry, соответственно.
-// Также, я уменьшил количество параметров в ParseCitySubjson, передав только необходимые параметры.
